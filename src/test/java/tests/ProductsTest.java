ca2e50cb23b07768bfa237a5d6a6f36079a2bec3
TMFZ <temur01@yandex.ru>
2026-01-27 21:46:53 +0300

    Список изменён на приметивный массив

--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -12,14 +12,14 @@ import static org.testng.Assert.assertTrue;
 
 public class ProductsTest extends BaseTest {
     private Random random;
-    private List<String> products = Arrays.asList("Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Bolt T-Shirt", "Sauce Labs Fleece Jacket", "Sauce Labs Onesie", "Test.allTheThings() T-Shirt (Red)");
+    private String[] products = {"Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Bolt T-Shirt", "Sauce Labs Fleece Jacket", "Sauce Labs Onesie", "Test.allTheThings() T-Shirt (Red)"};
 
     @Test(testName = "Добавление случайного товара в корзину")
     public void addRandProductTest() {
         random = new Random();
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-        String randomProduct = products.get(random.nextInt(products.size()));
+        String randomProduct = products[random.nextInt(products.length)];
         productsPage.addToCart(randomProduct);
         assertTrue(productsPage.checkVisibilityCartCounter(), "Счётчик корзины не появился на страницы");
         assertEquals(productsPage.checkCartCounter(), "1");
@@ -29,7 +29,7 @@ public class ProductsTest extends BaseTest {
     public void addToCartTwoProductsTest() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-        productsPage.addToCart(products.get(2), products.get(5));
+        productsPage.addToCart(products[2], products[5]);
         assertTrue(productsPage.checkVisibilityCartCounter(), "Счётчик корзины не появился на страницы");
         assertEquals(productsPage.checkCartCounter(), "2");
     }
