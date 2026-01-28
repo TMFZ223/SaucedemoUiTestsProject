bb1ab344a4df1432dfb207b5db5d0eb01301a88b
TMFZ <temur01@yandex.ru>
2026-01-28 15:00:49 +0300

    Исправлена инициализация Random и список товаров перенесён в неизменяемый список

--- src/test/java/tests/ProductsTest.java
+++ src/test/java/tests/ProductsTest.java
@@ -1,24 +1,21 @@
 package tests;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 import static org.testng.Assert.*;
 
 public class ProductsTest extends BaseTest {
-    private Random random;
-    private String[] products = {"Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Bolt T-Shirt", "Sauce Labs Fleece Jacket", "Sauce Labs Onesie", "Test.allTheThings() T-Shirt (Red)"};
+    private Random random = new Random();
+    private final List<String> expectedProducts = List.of("Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Bolt T-Shirt", "Sauce Labs Fleece Jacket", "Sauce Labs Onesie", "Test.allTheThings() T-Shirt (Red)");
 
     @Test(testName = "Добавление случайного товара в корзину")
     public void addRandProductTest() {
-        random = new Random();
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-        String randomProduct = products[random.nextInt(products.length)];
+        String randomProduct = expectedProducts.get(random.nextInt(expectedProducts.size()));
         productsPage.addToCart(randomProduct);
         assertTrue(productsPage.checkVisibilityCartCounter(), "Счётчик корзины не появился на страницы");
         assertEquals(productsPage.checkCartCounter(), "1");
@@ -28,17 +25,16 @@ public class ProductsTest extends BaseTest {
     public void addToCartTwoProductsTest() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-        productsPage.addToCart(products[2], products[5]);
+        productsPage.addToCart(expectedProducts.get(2), expectedProducts.get(5));
         assertTrue(productsPage.checkVisibilityCartCounter(), "Счётчик корзины не появился на страницы");
         assertEquals(productsPage.checkCartCounter(), "2");
     }
 
     @Test(testName = "Добавление и удаление случайного товара из корзины")
     public void addAndDeleteRandProductTest() {
-        random = new Random();
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-        String randomProduct = products[random.nextInt(products.length)];
+        String randomProduct = expectedProducts.get(random.nextInt(expectedProducts.size()));
         productsPage.addToCart(randomProduct);
         assertEquals(productsPage.checkCartText(), "1");
         productsPage.removeFromCart(randomProduct);
@@ -49,9 +45,9 @@ public class ProductsTest extends BaseTest {
     public void addAndRemoveFromCartTwoProductsTest() {
         loginPage.open();
         loginPage.login("standard_user", "secret_sauce");
-        productsPage.addToCart(products[1], products[3]);
+        productsPage.addToCart(expectedProducts.get(1), expectedProducts.get(3));
         assertEquals(productsPage.checkCartText(), "2");
-        productsPage.removeFromCart(products[1], products[3]);
+        productsPage.removeFromCart(expectedProducts.get(1), expectedProducts.get(3));
         assertEquals(productsPage.checkCartText(), "");
     }
 }
