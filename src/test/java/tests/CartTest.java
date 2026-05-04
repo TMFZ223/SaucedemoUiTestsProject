79d0ecfd0fad2e412fa5410822f468724d4e5769
TMFZ <temur01@yandex.ru>
2026-05-04 16:29:34 +0300

    Добавлены soft asserts

--- src/test/java/tests/CartTest.java
+++ src/test/java/tests/CartTest.java
@@ -4,24 +4,22 @@ import io.qameta.allure.Feature;
 import org.testng.annotations.Test;
 import user.UserFactory;
 
-import java.util.List;
 import java.util.Random;
 
 import static enums.PageTitles.Cart;
-import static org.testng.Assert.*;
 
 @Feature("Работа с корзиной товаров")
 public class CartTest extends BaseTest {
     private Random random = new Random();
-    private final List<String> expectedProducts = List.of("Sauce Labs Backpack", "Sauce Labs Bike Light", "Sauce Labs Bolt T-Shirt", "Sauce Labs Fleece Jacket", "Sauce Labs Onesie", "Test.allTheThings() T-Shirt (Red)");
 
     @Test(description = "Переход в корзину")
     public void testGoCart() {
         loginPage.open();
         loginPage.login(UserFactory.withAdminPermission());
         productsPage.swichToCart();
-        assertTrue(cartPage.checkDisplayingTitle());
-        assertEquals(cartPage.checkTitleText(), Cart.getDisplayName());
+        soft.assertTrue(cartPage.checkDisplayingTitle());
+        soft.assertEquals(cartPage.checkTitleText(), Cart.getDisplayName());
+        soft.assertAll();
     }
 
     @Test(description = "Добавление и просмотр товара в корзине")
@@ -31,8 +29,9 @@ public class CartTest extends BaseTest {
         String randomProduct = expectedProducts.get(random.nextInt(expectedProducts.size()));
         productsPage.addToCart(randomProduct);
         productsPage.swichToCart();
-        assertEquals(cartPage.getProductNames().size(), 1);
-        assertTrue(cartPage.getProductNames().contains(randomProduct));
+        soft.assertEquals(cartPage.getProductNames().size(), 1);
+        soft.assertTrue(cartPage.getProductNames().contains(randomProduct));
+        soft.assertAll();
     }
 
     @Test(description = "Добавление и просмотр двух товаров в корзине")
@@ -41,8 +40,9 @@ public class CartTest extends BaseTest {
         loginPage.login(UserFactory.withAdminPermission());
         productsPage.addToCart(expectedProducts.get(2), expectedProducts.get(5));
         productsPage.swichToCart();
-        assertEquals(cartPage.getProductNames().size(), 2);
-        assertTrue(cartPage.getProductNames().contains(expectedProducts.get(2)) && cartPage.getProductNames().contains(expectedProducts.get(5)));
+        soft.assertEquals(cartPage.getProductNames().size(), 2);
+        soft.assertTrue(cartPage.getProductNames().contains(expectedProducts.get(2)) && cartPage.getProductNames().contains(expectedProducts.get(5)));
+        soft.assertAll();
     }
 
     @Test(description = "Продолжение покупок после добавления товара в корзину")
@@ -54,7 +54,8 @@ public class CartTest extends BaseTest {
         cartPage.clickContinueShopping();
         productsPage.addToCart(expectedProducts.get(3));
         productsPage.swichToCart();
-        assertEquals(cartPage.getProductNames().size(), 2);
-        assertTrue(cartPage.getProductNames().contains(expectedProducts.get(0)) && cartPage.getProductNames().contains(expectedProducts.get(3)));
+        soft.assertEquals(cartPage.getProductNames().size(), 2);
+        soft.assertTrue(cartPage.getProductNames().contains(expectedProducts.get(0)) && cartPage.getProductNames().contains(expectedProducts.get(3)));
+        soft.assertAll();
     }
 }
